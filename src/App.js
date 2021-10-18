c2df07abc044985ddfff3b4288835d3acc4c4ad0
tajmin <tajminur.aiub@gmail.com>
2021-10-18 11:09:53 +0600

    nav integration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Header from './components/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is tailwind</h1>
+      <BrowserRouter>
+        <Header></Header>
+      </BrowserRouter>
     </div>
   );
 }
