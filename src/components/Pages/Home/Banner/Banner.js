76aa1074a380e10f901a241affc3fad895e26dca
tajmin <tajminur.aiub@gmail.com>
2021-10-18 13:45:53 +0600

    banner responsive fix

--- src/components/Pages/Home/Banner/Banner.js
+++ src/components/Pages/Home/Banner/Banner.js
@@ -7,13 +7,13 @@ const Banner = () => {
         <div id="top-banner">
             <div className="xl:container mx-auto" >
                 <div>
-                    <div className="bg-red-500 max-w-lg py-16 text-center">
-                        <h1 className="text-6xl text-white font-bold uppercase tracking-wider">emergency</h1>
-                        <h2 className="text-3xl pt-4 font-semibold text-white">+1 909 3030 3022</h2>
+                    <div className="bg-red-500 md:max-w-lg py-4 md:py-16 text-center">
+                        <h1 className="text-2xl md:text-5xl text-white font-bold uppercase lg:tracking-wider">emergency</h1>
+                        <h2 className="text-xl md:text-3xl pt-4 font-semibold text-white">+1 909 3030 3022</h2>
                     </div>
                     <div className="bg-transparent max-w-lg py-16 text-center">
-                        <h1 className="text-8xl font-bold tracking-wider font-serif my-3">OneCare</h1>
-                        <h2 className="italic font-semibold text-2xl text-gray-500">Specialized in cancer treatement.</h2>
+                        <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold tracking-wider font-serif my-3">OneCare</h1>
+                        <h2 className="italic font-semibold md:text-2xl text-gray-500 break-words">Specialized in cancer treatement.</h2>
                     </div>
                 </div>
             </div>
