3955842c75e3c513d8f854e639522c974c155b2f
tajmin <tajminur.aiub@gmail.com>
2021-10-18 22:45:14 +0600

    service details - responsive

--- src/components/Pages/ServiceInfo/ServiceInfo.js
+++ src/components/Pages/ServiceInfo/ServiceInfo.js
@@ -1,34 +1,40 @@
+import { ChevronLeftIcon } from '@heroicons/react/outline';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 import useServices from '../../../hooks/useServices';
 
 const ServiceInfo = () => {
     const { serviceID } = useParams();
     const [services] = useServices();
     const [detailedService, setDetailedService] = useState();
-    console.log(serviceID);
+    const history = useHistory();
 
     useEffect(() => {
         const retrievedService = services?.find(item => item.id.toString() === serviceID);
         setDetailedService(retrievedService);
     }, [services])
 
+    const handleBack = () => {
+        history.goBack();
+    }
+
     return (
         <div className="bg-gray-100">
             <div className="container bg-white mx-auto">
                 <div className="max-w-screen-lg mx-auto xl:py-16">
-                    <div className="xl:py-10">
+                    <div className="py-10 xl:px-0 px-6">
                         <h1 className="text-2xl xl:text-5xl uppercase text-gray-600 mb-3">
                             {detailedService?.title}</h1>
                         <hr />
-                        <p className="xl:pt-10 pb-6 text-lg text-gray-500 leading-relaxed">{detailedService?.description.slice(0, 199)}-</p>
+                        <p className="pt-10 pb-6 xl:text-lg text-gray-500 leading-relaxed">{detailedService?.description.slice(0, 270)}-</p>
                     </div>
                     <div>
                         <img src={detailedService?.image} alt="" />
                     </div>
-                    <div className="xl:py-12 text-lg text-gray-500 leading-relaxed">
-                        <p>{detailedService?.description.slice(200)}</p>
+                    <div className="py-12 xl:px-0 px-6 text-lg text-gray-500 leading-relaxed">
+                        <p>{detailedService?.description.slice(250)}</p>
                     </div>
+                    <button onClick={handleBack} className="bg-pink-400 px-4 py-1.5 text-white text-lg mb-4"><ChevronLeftIcon className="mr-2 inline-block w-6"></ChevronLeftIcon>Back</button>
                 </div>
             </div>
         </div>
