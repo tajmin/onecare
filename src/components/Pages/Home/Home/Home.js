c8f93c47f990b9774fe87ec1ee272b0e3715dd6e
tajmin <tajminur.aiub@gmail.com>
2021-10-19 19:35:48 +0600

    subscription integrated

--- src/components/Pages/Home/Home/Home.js
+++ src/components/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
+import Subscription from '../Subscription/Subscription';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Services></Services>
+            <Subscription></Subscription>
         </div>
     );
 };
---
+++ src/components/Pages/Home/Subscription/Subscription.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Subscription = () => {
+    return (
+        <div className="bg-pink-400 flex">
+            <div className="m-auto py-16 w-2/3 md:w-1/4">
+                <h1 className="text-xl md:text-3xl text-center mb-4 text-white">Subscribe to Our Newsletter</h1>
+                <p className="text-xl text-gray-300 mb-8 text-center">Stay Updated on Latest Cancer Breakthrough</p>
+                <input className="w-full py-2 px-4 rounded" type="email" placeholder="Enter your email here" />
+            </div>
+        </div>
+    );
+};
+
+export default Subscription;
