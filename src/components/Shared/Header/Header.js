6b82b0a67fab383fd1d48ddb4833bd81127e82f7
tajmin <tajminur.aiub@gmail.com>
2021-10-19 15:09:42 +0600

    environment vars

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -49,8 +49,8 @@ const Header = () => {
                                         alt="Workflow"
                                     />
                                 </div>
-
                             </div>
+
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                                 <div className="hidden sm:block sm:ml-6">
                                     <div className="flex space-x-4">
