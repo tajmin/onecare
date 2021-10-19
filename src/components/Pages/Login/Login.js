e6054d7be84a09a1428b49c895ae146ab20bcfe9
tajmin <tajminur.aiub@gmail.com>
2021-10-19 10:24:34 +0600

    auth redirect

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useHistory, useLocation } from 'react-router';
 import useAuth from '../../../hooks/useAuth';
 
 
 const Login = () => {
     const { user, signInUsingGoogle } = useAuth();
+    const location = useLocation();
+    const history = useHistory();
+    const redirectUrl = location.state?.from || '/home';
 
+    const handleGoogleLogin = () => {
+        signInUsingGoogle().then(result => {
+            history.push(redirectUrl);
+        })
+    }
 
     return (
         <div>
-            <button onClick={signInUsingGoogle} className="px-8 bg-black text-white py-2">Google Sign In</button>
+            <button onClick={handleGoogleLogin} className="px-8 bg-black text-white py-2">Google Sign In</button>
         </div>
     );
 };
